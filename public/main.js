57de04b45f2a970fdf563c9e58bbcd9134e598d1
KillahBea <bee.noelle6@gmail.com>
2019-04-14 21:44:39 -0400

    push

--- public/main.js
+++ public/main.js
@@ -1,7 +1,44 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+let score = 0
+
+const addPoint = event => {
+  score += 1
+  let message = `${score}`
+  const teamOneScore = document.querySelector('.team1Score')
+  teamOneScore.textContent = message
+}
+const subtractPoint = event => {
+  score -= 1
+  let message = `${score}`
+  const teamOneScore = document.querySelector('.team1Score')
+  teamOneScore.textContent = message
 }
 
+let team2Score = 0
+
+const teamTwoAddPoint = event => {
+  team2Score += 1
+  let message = `${team2Score}`
+  const teamTwoScore = document.querySelector('.team2Score')
+  teamTwoScore.textContent = message
+}
+const teamTwoSubtractPoint = event => {
+  team2Score -= 1
+  let message = `${team2Score}`
+  const teamTwoScore = document.querySelector('.team2Score')
+  teamTwoScore.textContent = message
+}
+
+const main = () => {
+  const teamOneAdd = document.querySelector('.team-1-add-1-button')
+  teamOneAdd.addEventListener('click', addPoint)
+
+  const teamOneSubtract = document.querySelector('.team-1-subtract-1-button')
+  teamOneSubtract.addEventListener('click', subtractPoint)
+
+  const teamTwoAdd = document.querySelector('.team-2-add-1-button')
+  teamTwoAdd.addEventListener('click', teamTwoAddPoint)
+
+  const teamTwoSubtract = document.querySelector('.team-2-subtract-1-button')
+  teamTwoSubtract.addEventListener('click', teamTwoSubtractPoint)
+}
 document.addEventListener('DOMContentLoaded', main)
